bc2213fa7653f300a27d843a23d4d77675a5cb93
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-02-18 12:27:14 +0000

    added more questions and linked to the readme file

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-// const generateMarkdown = require("./challenge/starter/utils/generateMarkdown");
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // array of questions for user
 const questions = [
-//     const inquirer = require('inquirer');
 
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          message: 'What is the project title?',
-          name: 'projectTitle',
-        },
-        {
-          type: 'input',
-          message: 'Please enter the description',
-          name: 'description',
-        },
-        {
-          type: 'input',
-          message: 'Describe the installation process?',
-          name: 'installation',
-        },
-        {
-          type: 'input',
-          message: 'Please describe the usage',
-          name: 'usage',
-        },
-        {
-          type: 'input',
-          message: 'Can anyone contribute?',
-          name: 'contribution',
-        },
-        {
-          type: 'input',
-          message: 'Are there any tests',
-          name: 'tests',
-        },
 
-      ])
-      .then((response) => {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the project title? (Required)',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Please enter the description',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Describe the installation process?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please describe the usage',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Can anyone contribute, if yes what are the guidelines?',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'Are there any tests required?',
+        name: 'tests',
+      },
+      {
+        type: 'list',
+        message: 'What license does your repo is using?',
+        name: 'license',
+        choices: ["agpl", "MIT", "apache", "none"]
+      },
+      {
+        type: 'input',
+        message: 'What is your github user name?',
+        name: 'gitUserName',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'emailAddress',
+      },
 
-        // console.log(response.projectTitle)
-  
-        const readmeInfo = `
+    ])
+    .then((response) => {
+
+      // console.log(response.projectTitle)
+
+      const readmeInfo = `
   ${response.projectTitle}
   ${response.description}
   ${response.installation}
@@ -56,20 +73,17 @@ const questions = [
   
   
   `;
-  
-  
-        fs.writeFile('response.md', readmeInfo, (err) =>
-            err ? console.error(err) : console.log('README Completed!')
-        )
+//Function to store the response in the response.md file
+
+      fs.writeFile('response.md', readmeInfo, (err) =>
+        err ? console.error(err) : console.log('README Completed!')
+      )
     })
 
-    ];
-  
+];
 
 
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
+
 
 // // function to initialize program
 // function init() {
