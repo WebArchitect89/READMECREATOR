4ff60c223733286415768879c4755f2d8563aafd
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-02-18 19:07:15 +0000

    added a function for the contribution senction

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,2 +1,17 @@
+//Displays a message depending of whether the developers want the contributors or not.
+
+function contributingReply(confirmContributers) {
+
+  if (confirmContributers) {
+    return `Thank you for your interest in helping out. Feel free to add your contributions.`
+  } else {
+  return `Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.`;
+  }
+
+}
+
+
+
+
 
 module.exports = generateMarkdown;
